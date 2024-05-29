365238266b2505c7d182b43ecd879132fc9b6a00
AndreWolf <sabou.andrei26@gmail.com>
2024-05-29 16:44:48 +0300

    render Cart in Isolation

--- ecomm/cart/src/bootstrap.js
+++ ecomm/cart/src/bootstrap.js
@@ -1,5 +1,21 @@
 import facker from 'faker';
 
-const cartText = `<div> You have ${facker.random.number()} items in your cart</div>`;
 
-document.querySelector('#dev-cart').innerHTML = cartText;
+const mount = (el) => {
+    const cartText = `<div> You have ${facker.random.number()} items in your cart</div>`;
+
+    el.innerHTML = cartText;
+}
+
+if (process.env.NODE_ENV === 'development') {
+    const el = document.querySelector('#dev-cart');
+    
+    // Assuming our continaer doesnt have an element with an id of 'dev-cart'
+    if (el) {
+        // We are probably running in isolation
+        mount(el);
+    
+    }
+}
+
+export { mount };
